5f41221d78cd8d14839204dd847b2f228ca6de67
Sergei Gavrish <sd.gavrish@gmail.com>
2020-04-26 20:47:30 +0300

    RM-2 App component was updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,24 @@ import React from 'react';
 
 import './App.css';
 
-import logo from './logo.svg';
-import ChatCard from './components/Chat/Card/ChatCard';
-import { IChatInfo } from './models/ichat-info';
-import { IMessage } from './models/imessage';
-import { IUser } from './models/iuser';
+import MainLayout from './core/MainLayout/MainLayout';
+import ChatList from './components/Chat/List/ChatList';
+import assembleMockState from './helpers/mockStateAssembler';
+import { IChatCardProps } from './components/Chat/Card/IChatCardProps';
 
-const chatStorage: { [chatId: string]: IChatInfo } = {
-  '82dde7de-c57f-40d6-b0e0-68acfd96e6e8': {
-    guid: '82dde7de-c57f-40d6-b0e0-68acfd96e6e8',
-    name: 'First chat',
-    logo,
-  },
-};
-
-const userStorage: { [userId: string]: IUser } = {
-  '3641c0d3-c497-4303-82af-2854f63fccdd': {
-    name: 'John Travolta',
-    avatar: logo,
-    guid: '3641c0d3-c497-4303-82af-2854f63fccdd',
-  },
-};
-
-const messageStorage: { [chatId: string]: IMessage[] } = {
-  '82dde7de-c57f-40d6-b0e0-68acfd96e6e8': [
-    {
-      timestamp: 1580816220000,
-      guid: 'ae503120-25c4-4508-b926-e718077f2915',
-      authorId: '3641c0d3-c497-4303-82af-2854f63fccdd',
-      content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure doloribus quae officia exercitationem, enim quasi iste facilis impedit in asperiores expedita dolor facere vel nisi cum molestiae nobis deleniti ratione.',
-    },
-  ],
-};
+const { chatStorage, userStorage, messageStorage } = assembleMockState();
 
 function App() {
-  const chatInfo = chatStorage['82dde7de-c57f-40d6-b0e0-68acfd96e6e8'];
-  const chatMessages = messageStorage['82dde7de-c57f-40d6-b0e0-68acfd96e6e8'];
-  const lastMessage = chatMessages[chatMessages.length - 1];
-  const author = userStorage[lastMessage.authorId];
-  const props = { ...chatInfo, lastMessage, author };
+  const props: IChatCardProps[] = Object.keys(chatStorage).map((chatId) => {
+    const chatInfo = chatStorage[chatId];
+    const chatMessages = messageStorage[chatInfo.guid];
+    const lastMessage = chatMessages[chatMessages.length - 1];
+    const author = userStorage[lastMessage.authorId];
+    return { ...chatInfo, lastMessage, author };
+  });
   return (
     <div className="App">
-      <ChatCard {...props} />
+      <MainLayout sidebar={<ChatList chats={props} />} />
     </div>
   );
 }
