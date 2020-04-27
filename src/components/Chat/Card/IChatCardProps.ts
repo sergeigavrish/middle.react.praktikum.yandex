48b9264873c7f015a00e1cd83e057186877d3118
Sergei Gavrish <sd.gavrish@gmail.com>
2020-04-27 16:03:03 +0300

    RM-4 ChatCard was updated

--- src/components/Chat/Card/IChatCardProps.ts
+++ src/components/Chat/Card/IChatCardProps.ts
@@ -1,8 +1,13 @@
+import { SyntheticEvent } from 'react';
+
 import { IChatInfo } from '../../../models/ichat-info';
 import { IMessage } from '../../../models/imessage';
 import { IUser } from '../../../models/iuser';
 
-export interface IChatCardProps extends IChatInfo {
+export interface IChatCardProps {
+  selected: boolean;
+  chat: IChatInfo;
   lastMessage: IMessage;
   author: IUser;
+  onChatSelected(e: SyntheticEvent<HTMLElement, MouseEvent>): void;
 }
