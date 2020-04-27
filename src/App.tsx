22588ca19044117a3ef5ffef270be13b8aa42c28
Sergei Gavrish <sd.gavrish@gmail.com>
2020-04-27 15:53:30 +0300

    RM-4 App component was updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,11 @@ import React from 'react';
 import './App.css';
 
 import MainLayout from './core/MainLayout/MainLayout';
-import ChatList from './components/Chat/List/ChatList';
-import assembleMockState from './helpers/mockStateAssembler';
-import { IChatCardProps } from './components/Chat/Card/IChatCardProps';
-import sortHelper from './helpers/sortHelper';
-import mapperHelper from './helpers/mapperHelper';
-
-const { chatStorage } = assembleMockState();
 
 function App() {
-  const props: IChatCardProps[] = mapperHelper
-    .mapChatInfoToChatCardProps(chatStorage)
-    .sort((a, b) => sortHelper.sortByDate(a.lastMessage, b.lastMessage));
   return (
     <div className="App">
-      <MainLayout sidebar={<ChatList chats={props} />} />
+      <MainLayout />
     </div>
   );
 }
