51f6eb21f02049b49245bb47e42494cb649bde22
Sergei Gavrish <sd.gavrish@gmail.com>
2020-04-27 16:03:20 +0300

    RM-4 MainLayout was updated

--- src/core/MainLayout/MainLayout.tsx
+++ src/core/MainLayout/MainLayout.tsx
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 
 import './MainLayout.css';
+import { IChatInfo } from '../../models/ichat-info';
+import { IUser } from '../../models/iuser';
+import { IMessage } from '../../models/imessage';
+import mockStateAssembler from '../../helpers/mockStateAssembler';
+import mapperHelper from '../../helpers/mapperHelper';
+import ChatList from '../../components/Chat/List/ChatList';
+import { IHashTable } from '../../models/ihash-table';
 
-interface IMainLayoutProps {
-  sidebar: React.ReactNode;
-  content?: React.ReactNode;
+interface IMainLayoutState {
+  selectedChat: null | string;
+  chats: IHashTable<IChatInfo>;
+  users: IHashTable<IUser>;
+  messages: IHashTable<IMessage[]>;
 }
 
-const MainLayout: React.FunctionComponent<IMainLayoutProps> = ({
-  sidebar,
-  content,
-}: IMainLayoutProps) => (
-  <main className="layout">
-    <aside className="layout__sidebar">{sidebar}</aside>
-    <div className="layout__content">{content}</div>
-  </main>
-);
+export default class MainLayout extends React.Component<{}, IMainLayoutState> {
+  constructor(props = {}) {
+    super(props);
+    this.state = {
+      selectedChat: null,
+      chats: {},
+      users: {},
+      messages: {},
+    };
+  }
 
-export default MainLayout;
+  async componentDidMount() {
+    const { userStorage, chatStorage, messageStorage } = await mockStateAssembler();
+    this.setState({
+      chats: chatStorage,
+      users: userStorage,
+      messages: messageStorage,
+    });
+  }
+
+  onChatSelected = (e: SyntheticEvent<HTMLElement, MouseEvent>) => {
+    this.setState({
+      selectedChat: e.currentTarget.dataset.guid as string,
+    });
+  }
+
+  render() {
+    const {
+      chats,
+      users,
+      messages,
+      selectedChat,
+    } = this.state;
+    const props = mapperHelper.mapChatInfoToChatCardProps(
+      chats,
+      users,
+      messages,
+      selectedChat,
+      this.onChatSelected,
+    );
+    return (
+      <main className="layout">
+        <aside className="layout__sidebar">
+          <ChatList chats={props} />
+        </aside>
+        <div className="layout__content" />
+      </main>
+    );
+  }
+}
