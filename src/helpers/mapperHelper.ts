9e7319cae8e0cf99c3e0476c149aeab2ce43c44d
Sergei Gavrish <sd.gavrish@gmail.com>
2020-04-28 02:43:46 +0300

    RM-5 helpers were updated

--- src/helpers/mapperHelper.ts
+++ src/helpers/mapperHelper.ts
@@ -1,9 +1,14 @@
 import { SyntheticEvent } from 'react';
+
+import { v4 as uuid } from 'uuid';
+
 import { IChatInfo } from '../models/ichat-info';
 import { IHashTable } from '../models/ihash-table';
 import { IUser } from '../models/iuser';
-import { IMessage } from '../models/imessage';
+import { IMessage, MessageList } from '../models/imessage';
 import { IChatCardProps } from '../components/Chat/Card/IChatCardProps';
+import { MessageTypes } from '../models/messageTypes';
+import dateHelper from './dateHelper';
 
 const mapChatInfoToChatCardProps = (
   chats: IHashTable<IChatInfo>,
@@ -27,9 +32,35 @@ const mapChatInfoToChatCardProps = (
     };
   });
 
+const mapMessageToMessageWithAuthor = (
+  messageList: IMessage[],
+  users: IHashTable<IUser>,
+): MessageList => {
+  const dateSet: Set<number> = new Set();
+  return messageList.reduce((acc, m) => {
+    const timestamp = dateHelper.removeTimeFromTimestamp(m.timestamp);
+    if (!dateSet.has(timestamp)) {
+      dateSet.add(timestamp);
+      acc.push({
+        guid: uuid(),
+        timestamp: m.timestamp,
+        type: MessageTypes.Service,
+      });
+    }
+    acc.push({
+      guid: m.guid,
+      timestamp: m.timestamp,
+      content: m.content,
+      author: users[m.authorId],
+      type: MessageTypes.Text,
+    });
+    return acc;
+  }, [] as MessageList);
+};
 
 const mapperHelper = {
   mapChatInfoToChatCardProps,
+  mapMessageToMessageWithAuthor,
 };
 
 export default mapperHelper;
