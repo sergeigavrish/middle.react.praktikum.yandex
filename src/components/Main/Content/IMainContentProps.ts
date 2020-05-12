48d94e4702d4e73925e33d0e98f9cd6bd320989c
Sergei Gavrish <sd.gavrish@gmail.com>
2020-05-13 00:33:03 +0300

    RM-12 MainContent was rewritten as FC

--- src/components/Main/Content/IMainContentProps.ts
+++
@@ -1,4 +0,0 @@
-import { RouteComponentProps } from 'react-router-dom';
-import { IWithQueryFromUrlInjectedProps } from '../../../shared/WithQueryFromUrl/IWithQueryFromUrlInjectedProps';
-
-export interface IMainContentProps extends RouteComponentProps, IWithQueryFromUrlInjectedProps { }
