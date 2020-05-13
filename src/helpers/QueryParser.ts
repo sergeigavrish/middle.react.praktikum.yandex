08dc15eb6e2a2511efb754a5d4b437300d03d51d
Sergei Gavrish <36244364+gogumaUno@users.noreply.github.com>
2020-05-13 20:01:33 +0300

    RM-15 Delete QueryParser.ts

--- src/helpers/QueryParser.ts
+++
@@ -1,6 +0,0 @@
-import { Parser } from '../models/entities/Parser';
-
-import { UrlQueryParams } from '../models/types/UrlQueryParams';
-import { QUERY_PATTERN } from '../models/constants/QUERY_PATTERN';
-
-export const queryParser = new Parser<UrlQueryParams>(UrlQueryParams, QUERY_PATTERN);
