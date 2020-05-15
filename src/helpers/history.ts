14af32177128692cfb91ff1fe802d8265ae4b653
Sergei Gavrish <sd.gavrish@gmail.com>
2020-05-15 04:18:38 +0300

    RM-16 helpers were updated

--- src/helpers/history.ts
+++ src/helpers/history.ts
@@ -1,7 +1,9 @@
 import { createHashHistory } from 'history';
 
+import { Routes } from '../models/types/Routes';
+
 export const history = createHashHistory();
 
-export function navigate(location: string) {
+export function navigate(location: Routes) {
   history.push(location);
 }
